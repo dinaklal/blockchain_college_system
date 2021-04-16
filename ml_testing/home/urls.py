32b70cc6f15345fe5de99435095d31e42f9b0fa8
DINAK LAL V <dinaklal@gmail.com>
2021-04-16 23:37:29 +0530

    admin

--- ml_testing/home/urls.py
+++ ml_testing/home/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('register', views.register, name='register'),
     path('view', views.view, name='view'),
+    path('change_status', views.change_status, name='change_status'),
 
 
 ]
--- ml_testing/home/views.py
+++ ml_testing/home/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User,auth
 from home.block import *
 from home.Blockchain import *
 from django.contrib import messages
-
+from collections import Counter
 import json
 
 from django.conf import settings
@@ -16,6 +16,7 @@ bc = Blockchain()
 
 def home(request):
     username = request.user.username
+
     if username == 'student_common_login':
       return render(request, 'home2.html')
     else:
@@ -26,6 +27,8 @@ def home(request):
               d = {}
               block = bc.get_block(b)
               d['student_id'] =block.student_id
+              d['height'] = block.height
+              d['timestamp'] = block.timestamp
               d['first_name'] = block.first_name
               d['last_name'] =block.last_name
               d['tuitionfee'] =block.tuition_fee
@@ -35,8 +38,55 @@ def home(request):
               d['status'] = block.status
               if (d['status'] != 0):
                 data.append(d)
-          return  render(request,'home.html',{'data':data})
-      
+          k = [x['student_id'] for x in data]
+
+          new_vals = []
+
+          for i in Counter(k):
+              all = [x for x in data if x['student_id'] == i]
+              new_vals.append(max(all, key=lambda x: x['timestamp']))
+          return  render(request,'home.html',{'data':new_vals})
+def change_status(request):
+    if request.method == "GET":
+        id = request.GET['id']
+        action = request.GET['action']
+        if action == '1':
+            temp  = bc.get_block(int(id))
+            temp.status = "Approved"
+            bc.add_block(temp)
+        else:
+            temp = bc.get_block(int(id))
+            temp.status = "Rejected"
+            bc.add_block(temp)
+
+        print(bc)
+        print(action)
+        m = ""
+        data = []
+
+        for b in range(bc.get_length()):
+            d = {}
+            block = bc.get_block(b)
+            d['student_id'] = block.student_id
+            d['height'] = block.height
+            d['timestamp'] = block.timestamp
+            d['first_name'] = block.first_name
+            d['last_name'] = block.last_name
+            d['tuitionfee'] = block.tuition_fee
+            d['messfee'] = block.mess_fee
+            d['hostelfee'] = block.hostel_fee
+            d['course'] = block.course
+            d['status'] = block.status
+            if (d['status'] != 0):
+                data.append(d)
+        k = [x['student_id'] for x in data]
+
+        new_vals = []
+
+        for i in Counter(k):
+            all = [x for x in data if x['student_id'] == i]
+            new_vals.append(max(all, key=lambda x: x['timestamp']))
+        return render(request, 'home.html', {'data': new_vals})
 def logout(request):
     auth.logout(request)
     return redirect('/')
@@ -62,6 +112,8 @@ def view(request):
         d = {}
         block = bc.get_block(b)
         d['student_id'] =block.student_id
+        d['height'] = block.height
+        d['timestamp'] = block.timestamp
         d['first_name'] = block.first_name
         d['last_name'] =block.last_name
         d['tuitionfee'] =block.tuition_fee
@@ -71,5 +123,12 @@ def view(request):
         d['status'] = block.status
         if (d['status'] != 0):
           data.append(d)
-    return  render(request,'view.html',{'data':data})
+    k = [x['student_id'] for x in data]
+
+    new_vals = []
+
+    for i in Counter(k):
+        all = [x for x in data if x['student_id'] == i]
+        new_vals.append(max(all, key=lambda x: x['timestamp']))
+    return  render(request,'view.html',{'data':new_vals})
 
